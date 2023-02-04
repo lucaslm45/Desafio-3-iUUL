0849b022a2058b4f72fdaad415a91dfe32e2fa0c
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2023-02-03 21:20:17 -0300

    Refatoração na classe OdontoDbContext
    - Cria o método IniciaBanco() na classe para tirar da classe Program
    - Adiciona configuração dos diretórios para a minha máquina

--- NovoOdonto/Program.cs
+++ NovoOdonto/Program.cs
@@ -12,55 +12,11 @@ namespace NovoOdonto
     {
         static void Main(string[] args)
         {
-            MainController controlador = new MainController();
+            var controlador = new MainController();
+            var  context = new OdontoDbContext();
 
-            var localizacaoPastaProjeto = "C:\\Users\\lucas\\Documents\\GitHub\\";
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile($"{localizacaoPastaProjeto}Desafio-3-iUUL\\NovoOdonto\\util\\appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-            var services = new ServiceCollection();
-
-            using var connection = new NpgsqlConnection(connectionString);
-
-            try
-            {
-                // O banco de Dados existe?
-                connection.Open();
-
-                services.AddDbContext<OdontoDbContext>(options =>
-                    options.UseNpgsql(configuration.GetConnectionString(connectionString)));
-            }
-            catch (NpgsqlException)
-            {
-                Console.WriteLine("O Banco de Dados não Existe.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message + $"Não possível efetuar a conexão com o Banco de " +
-                    $"Dados usando a string de conexão: {connectionString}");
-            }
-            finally
-            {
-                //ListaPacientes lista = new ListaPacientes();
-                //Paciente p1 = new Paciente(62813265047, "Marcos", "17/07/2001");
-                //lista.AdicionarNaLista(p1);
-
-                //Paciente p2 = new Paciente(92188860020, "Larissa", "01/02/2000");
-                //lista.AdicionarNaLista(p2);
-
-                ////Paciente p3 = new Paciente(92188860020, "Maria Helena", "04/10/1999");
-                ////lista.AdicionarNaLista(p3);
-
-                ////lista.RemoverDaLista(p3);
-
-                //lista.ListarPacientes();
-
-                controlador.Inicia();
-            }
-
+            context.IniciaBanco();
+            controlador.Inicia();
         }
 
     }
--- NovoOdonto/data/OdontoDbContext.cs
+++ NovoOdonto/data/OdontoDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using NovoOdonto.model;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -21,9 +23,48 @@ namespace NovoOdonto.data
 
         }
 
+        public void IniciaBanco()
+        {
+            //var localizacaoPastaProjeto = "C:\\Users\\lucas\\Documents\\GitHub\\";
+            var localizacaoPastaProjeto = "E:\\Residencia\\";
+            /*
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile($"{localizacaoPastaProjeto}Desafio-3-iUUL\\NovoOdonto\\util\\appsettings.json", optional: false, reloadOnChange: true)
+                .Build(); */
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile($"{localizacaoPastaProjeto}Desafio-3-iUUL\\NovoOdonto\\util\\appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var services = new ServiceCollection();
+
+            using var connection = new NpgsqlConnection(connectionString);
+
+            try
+            {
+                // O banco de Dados existe?
+                connection.Open();
+
+                services.AddDbContext<OdontoDbContext>(options =>
+                    options.UseNpgsql(configuration.GetConnectionString(connectionString)));
+            }
+            catch (NpgsqlException)
+            {
+                Console.WriteLine("O Banco de Dados não Existe.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + $"Não possível efetuar a conexão com o Banco de " +
+                    $"Dados usando a string de conexão: {connectionString}");
+            }
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var localizacaoPastaProjeto = "C:\\Users\\lucas\\Documents\\GitHub\\";
+            //var localizacaoPastaProjeto = "C:\\Users\\lucas\\Documents\\GitHub\\";
+            var localizacaoPastaProjeto = "E:\\Residencia\\";
+
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile($"{localizacaoPastaProjeto}Desafio-3-iUUL\\NovoOdonto\\util\\appsettings.json", optional: false, reloadOnChange: true)
                 .Build();
